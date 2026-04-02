cb8954431bb0ec11106953690201661b6a4918a9
Youssef Waleed <youssefwaleed827@gmail.com>
2026-04-02 16:59:24 +0200

    Iphone 13

---
+++ iphone.js
@@ -0,0 +1,8 @@
+let image=document.getElementById("img0");
+let cont=document.getElementById("parent");
+function phones(phone){
+    image.src=phone;
+}
+function colors(color){
+    cont.style.backgroundColor=color;
+} 
